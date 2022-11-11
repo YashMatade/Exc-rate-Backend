f464f732e558fe3ea7d91ba2a74fdb626c67076b
Yash Matade <105554335+YashMatade@users.noreply.github.com>
2022-11-11 17:13:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 });
 
 app.get("/", (req, res) => {
-    res.send("Hello mongodb");
+    res.end("Welcome to Currency Exchange Web App");
 });
 
 app.get("/sync", (req, res) => {
